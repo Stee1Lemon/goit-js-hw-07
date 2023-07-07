9ef0b5525fc740d69ed2de13023e46a3bf2beed5
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-07-07 19:20:30 +0100

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+};
+
+let finalGallery = "";
+
+createFinalGallery(galleryItems);
+
+function createFinalGallery(arrayImages) {
+  createGalleryMatCap(arrayImages);
+  refs.galleryEl.insertAdjacentHTML("beforeend", finalGallery);
+}
+
+function createGalleryMatCap(arrayImages) {
+  const galleryMatCap = [];
+  arrayImages.map((image) => {
+    const imageEl = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+            </a>
+         </li>`;
+    galleryMatCap.push(imageEl);
+  });
+  finalGallery = galleryMatCap.join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
